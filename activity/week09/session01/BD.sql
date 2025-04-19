08ddab63e92b776d7d8edf4bef2d83669810dd43
karina cantillo plaza <karinaccpp@gmail.com>
2025-04-19 00:06:27 -0500

    week09

--- activity/week09/session01/BD.sql
+++ activity/week09/session01/BD.sql
@@ -29,7 +29,7 @@ CREATE TABLE rol(
     descripcion TEXT NOT NULL
 );
 
-CREATE table persona_rol(
+CREATE table perona_rol(
     id INT PRIMARY KEY AUTO_INCREMENT,
     persona_id INT NOT NULL,
     rol_id INT NOT NULL,
@@ -76,55 +76,27 @@ create table evento(
 );
 
 
--- DML 
+-- DML
 
--- Insert 10 personas
-INSERT INTO persona (nombre, edad, correo, telefono) VALUES
-('Juan Perez', '35', 'juan.perez@example.com', '1234567890'),
-('Maria Lopez', '28', 'maria.lopez@example.com', '0987654321'),
-('Carlos Gomez', '40', 'carlos.gomez@example.com', '1122334455'),
-('Ana Torres', '30', 'ana.torres@example.com', '2233445566'),
-('Luis Martinez', '45', 'luis.martinez@example.com', '3344556677'),
-('Sofia Ramirez', '25', 'sofia.ramirez@example.com', '4455667788'),
-('Miguel Sanchez', '50', 'miguel.sanchez@example.com', '5566778899'),
-('Laura Diaz', '32', 'laura.diaz@example.com', '6677889900'),
-('Jorge Herrera', '38', 'jorge.herrera@example.com', '7788990011'),
-('Elena Cruz', '29', 'elena.cruz@example.com', '8899001122');
-
--- Insert roles
-INSERT INTO rol (nombre, descripcion) VALUES
-('Director', 'Encargado de supervisar y dirigir la producción'),
-('Guionista', 'Responsable de escribir el guion de la producción'),
-('Camarógrafo', 'Encargado de operar la cámara durante la filmación'),
-('Editor', 'Responsable de editar el material grabado'),
-('Técnico de sonido', 'Encargado de grabar y mezclar el sonido de la producción');
-
-insert into persona_rol(
-    persona_id,
-    rol_id
+insert into persona(
+    nombre,
+    edad,
+    correo,
+    telefono
 )VALUES
-(1, 1),
-(1, 3),
-(1, 4),
-(2, 3),
-(3, 1),
-(4, 4),
-(6, 1),
-(6, 3),
-(6, 2),
-(6, 4),
-(6, 5);
+("Juan Perez","25","juan@gmail.com","123456789"),
+("Maria Lopez","30","maria@gmail.com","987654321"),
+("Carlos Gomez","28","carlos@gmail.com","456123789"),
+("Ana Torres","35","ana@gmail.com","321654987"),
+("Luis Martinez","40","luis@gmail.com","654987321"),
+("Sofia Ramirez","22","sofia@gmail.com","789123456"),
+("Miguel Sanchez","33","miguel@gmail.com","123789456"),
+("Laura Diaz","27","laura@gmail.com","987321654"),
+("Jorge Herrera","29","jorge@gmail.com","654123987"),
+("Elena Cruz","31","elena@gmail.com","456987123");
 
-
-select * from persona;
-select * from rol; 
-select * from persona_rol;
-
-select 
-	p.nombre persona,
-    r.nombre rol
-from 
-	persona p
-    inner join persona_rol pr on p.id = pr.persona_id
-    inner join rol r on pr.rol_id = r.id
-where p.id = 6;
+insert into rol(
+    nombre,
+    descripcion
+)VALUES
+;
